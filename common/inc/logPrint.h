176951cf02af7ccbe62b947c915b1189408c3129
ZhuZilong <zhuzilong2018@163.com>
2020-04-15 21:57:44 +0800

    init project

---
+++ common/inc/logPrint.h
@@ -0,0 +1,16 @@
+#ifndef _LOG_PRINT_H
+#define _LOG_PRINT_H
+
+#ifdef __cplusplus 
+extern "C" { 
+#endif 
+
+#endif
+
+
+
+#ifdef __cplusplus 
+}
+#endif
+
+#endif
---
+++ readme.txt
@@ -0,0 +1,10 @@
+[文件夹概述]
+common:
+    存放client和server都用到得源码文件
+client：
+    存放客户端源码
+server：
+    存放服务端源码
+target：
+    存放编译生成得库文件和最终编译文件
+
