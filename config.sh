fddc7ace0648be48107f1452bb006f4111e1cca4
xzsir <1404207953@qq.com>
2020-04-21 22:38:19 +0800

    config.sh

---
+++ config.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+FILE="config.mk"
+COMMON=$(pwd)/common
+SERVER=$(pwd)/server
+CLIENT=$(pwd)/client
+OUTPUT=$(pwd)/target
+COMM_INCLUDE=$(pwd)/commo/inc
+RELEASE=$(pwd)/target/release
+RELEASE_LIB=$(pwd)/target/lib
+RELEASE_BIN=$(pwd)/target/bin
+echo COMMON=$COMMON >> $FILE
+echo SERVER=$SERVER >> $FILE
+echo CLIENT=$CLIENT >> $FILE
+echo OUTPUT=$OUTPUT >> $FILE
+echo COMM_INCLUDE=$COMM_INCLUDE >> $FILE
+echo CC=gcc >> $FILE
+echo STRIP=strip >> $FILE
+echo AR=ar >> $FILE
+echo CXX=g++ >> $FILE
+echo RELEASE=$RELEASE >> $FILE
+echo RELEASE_LIB=$RELEASE_LIB >> $FILE
+echo RELEASE_BIN=$RELEASE_BIN >> $FILE
