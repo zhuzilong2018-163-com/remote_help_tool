a6225059aeabb200298c982ae3196f02fb54b78e
xz <1404207953@qq.com>
2020-04-23 23:25:00 +0800

    mode select

--- client/clientMain/client.c
+++ client/clientMain/client.c
@@ -2,23 +2,26 @@
 #include "stdlib.h"
 #include "string.h"
 #include "logPrint.h"
+#include "client.h"
 
-//宏定义
-//模式选择相关
-#define HELPER "helper"
-#define BENEFICIARY "beneficiary"
-
-
+//全局变量
+//模式判断变量
+unsigned char g_mode = 0;
 
 int main(int argc, char *argv[])
 {
-	if ((argc == 2) && (strcmp(HELPER, argv[1]) == 0)) {
-		
-	} else if ((argc == 2) && (strcmp(HELPER, argv[1]) == 0)) {
-		    
-	} else {
-		ERROR_LOG("client", "Parameter error");
-	}
-
-	return 0;
+    if ((argc == 2) && (strcmp(HELPER, argv[1]) == 0)) {
+        g_mode = E_MODE_HELPER;
+	    printf("You select Helper mode\n");
+        INFO_LOG(CLIENT, "%s\n","You select Helper mode\n");
+    } else if ((argc == 2) && (strcmp(HELPER, argv[1]) == 0)) {
+        g_mode = E_MODE_BENEFICIARY;
+	    printf("You select Beneficiary mode\n");
+        INFO_LOG(CLIENT, "%s\n","You select Beneficiary mode\n");
+    } else {
+        printf("Parameter error\n");
+        ERROR_LOG(CLIENT, "%s\n","Parameter error\n");
+    }
+    
+    return 0;
 }
--- client/inc/client.h
+++ client/inc/client.h
@@ -1,1 +1,31 @@
-#include "connect.h"
+#ifndef _CLIENT_H_
+#define _CLIENT_H_
+
+#ifdef __cplusplus 
+extern "C" { 
+#endif 
+
+//宏定义
+//模式选择相关
+#define HELPER "helper"
+#define BENEFICIARY "beneficiary"
+
+//调试相关
+#define CLIENT "CLINET"
+
+//枚举
+//模式选择
+typedef enum {
+	E_MODE_HELPER = 1,      //HELPER模式
+	E_MODE_BENEFICIARY = 2, //BENEFICIARY模式
+	E_MODE_BUTT,
+} E_CLIENT_TYPE;
+
+
+
+#ifdef __cplusplus 
+}
+#endif
+
+#endif
+
