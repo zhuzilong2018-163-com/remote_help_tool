2bbfa8163a3f8ed8884c8836e37cd14a3ab2e1bd
ltx1472225183 <1472225183@qq.com>
2020-04-24 22:26:36 +0800

    add typeinit.h to inc

---
+++ common/inc/typeinit.h
@@ -0,0 +1,7 @@
+typedef   signed          char u8
+typedef   signed short     int u16
+typedef   signed           int u32
+
+typedef unsigned          char uint8
+typedef unsigned short     int uint16
+typedef unsigned           int uint32
